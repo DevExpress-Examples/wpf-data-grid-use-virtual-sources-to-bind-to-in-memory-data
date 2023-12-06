6f8588fd817764929173d8907f2535d06514b57e
DevExpressExampleBot <fakeEmail>
2023-12-06 14:41:19 +0400

    Source auto update [skip ci]

--- VB/Data/IssueData.vb
+++ VB/Data/IssueData.vb
@@ -1,65 +1,75 @@
-﻿Imports System
+Imports System
 
 Namespace InfiniteSourceInMemoryData
-	Public Class IssueData
-		Public Sub New(ByVal subject As String, ByVal user As String, ByVal created As DateTime, ByVal votes As Integer, ByVal priority As Priority)
-			Me.Subject = subject
-			Me.User = user
-			Me.Created = created
-			Me.Votes = votes
-			Me.Priority = priority
-		End Sub
-		Private privateSubject As String
-		Public Property Subject() As String
-			Get
-				Return privateSubject
-			End Get
-			Private Set(ByVal value As String)
-				privateSubject = value
-			End Set
-		End Property
-		Private privateUser As String
-		Public Property User() As String
-			Get
-				Return privateUser
-			End Get
-			Private Set(ByVal value As String)
-				privateUser = value
-			End Set
-		End Property
-		Private privateCreated As DateTime
-		Public Property Created() As DateTime
-			Get
-				Return privateCreated
-			End Get
-			Private Set(ByVal value As DateTime)
-				privateCreated = value
-			End Set
-		End Property
-		Private privateVotes As Integer
-		Public Property Votes() As Integer
-			Get
-				Return privateVotes
-			End Get
-			Private Set(ByVal value As Integer)
-				privateVotes = value
-			End Set
-		End Property
-		Private privatePriority As Priority
-		Public Property Priority() As Priority
-			Get
-				Return privatePriority
-			End Get
-			Private Set(ByVal value As Priority)
-				privatePriority = value
-			End Set
-		End Property
-	End Class
-	Public Enum Priority
-		Low
-		BelowNormal
-		Normal
-		AboveNormal
-		High
-	End Enum
+
+    Public Class IssueData
+
+        Private _Subject As String, _User As String, _Created As DateTime, _Votes As Integer, _Priority As Priority
+
+        Public Sub New(ByVal subject As String, ByVal user As String, ByVal created As System.DateTime, ByVal votes As Integer, ByVal priority As InfiniteSourceInMemoryData.Priority)
+            Me.Subject = subject
+            Me.User = user
+            Me.Created = created
+            Me.Votes = votes
+            Me.Priority = priority
+        End Sub
+
+        Public Property Subject As String
+            Get
+                Return _Subject
+            End Get
+
+            Private Set(ByVal value As String)
+                _Subject = value
+            End Set
+        End Property
+
+        Public Property User As String
+            Get
+                Return _User
+            End Get
+
+            Private Set(ByVal value As String)
+                _User = value
+            End Set
+        End Property
+
+        Public Property Created As DateTime
+            Get
+                Return _Created
+            End Get
+
+            Private Set(ByVal value As DateTime)
+                _Created = value
+            End Set
+        End Property
+
+        Public Property Votes As Integer
+            Get
+                Return _Votes
+            End Get
+
+            Private Set(ByVal value As Integer)
+                _Votes = value
+            End Set
+        End Property
+
+        Public Property Priority As Priority
+            Get
+                Return _Priority
+            End Get
+
+            Private Set(ByVal value As Priority)
+                _Priority = value
+            End Set
+        End Property
+    End Class
+
+    Public Enum Priority
+        Low
+        BelowNormal
+        Normal
+        AboveNormal
+        High
+    End Enum
 End Namespace
